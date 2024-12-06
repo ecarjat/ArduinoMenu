2b761648e39fe20acfa4354f6ef548863dfbf66e
ecarjat <emmanuel.carjat@gmail.com>
2024-12-06 13:51:42 +0100

    Update chainStream.h

--- src/menuIO/chainStream.h
+++ src/menuIO/chainStream.h
@@ -13,50 +13,43 @@ scan a chain of several input streams to provide input
 
   namespace Menu {
 
-    template <int N>
-    class chainStream:public menuIn {
-      public:
-        static int on;
-        menuIn** streams;
-        chainStream<N>(menuIn** chain):streams(chain) {}
-        void setFieldMode(bool mode) override {
-          menuIn::setFieldMode(mode);
-          for(int n=0;n<N;n++) streams[n]->setFieldMode(mode);
-        }
-        int available(void) {
-          int cnt=0;
-          for(int n=0;n<N;n++)
-            cnt+=streams[n]->available();
-          return cnt;
-        }
-        int peek(void) {
-          for(int n=0;n<N;n++)
-            if (streams[n]->available()) return streams[n]->peek();
-          return -1;
-        }
-        int read() {
-          for(int n=0;n<N;n++) {
-            int key=streams[n]->available()?streams[n]->read():-1;
-            if (key!=on) {
-              on=-1;//key;
-              return key;
-            }
-              //streams[n]->read();
-              //if (key==-1) return -1;
-              //while(streams[n]->peek()==key) streams[n]->read();//wait for key release
-              //return key;
-          }
-          return -1;
-        }
-        void flush() {
-          for(int n=0;n<N;n++)
-            streams[n]->flush();
-        }
-        size_t write(uint8_t v) {return 0;}//this is readonly, ignoring
-    };
+    tnamespace Menu {
 
-    template<int N>
-    int chainStream<N>::on=-1;
+  template<int N>
+  class chainStream:public menuIn {
+    public:
+    menuIn* const* const inputs;
+    chainStream(menuIn* const* inputs):inputs(inputs) {}
+    int available(void) override {
+      for(int n=0;n<N;n++) {
+        int r=inputs[n]->available();
+        if (r) return r;
+      }
+      return 0;
+    }
+    int peek(void) override {
+      for(int n=0;n<N;n++) {
+        int r=inputs[n]->peek();
+        if (r>=0) return r;
+      }
+      return -1;
+    }
+    int read(void) override {
+      for(int n=0;n<N;n++) {
+        int r=inputs[n]->read();
+        if (r>=0) return r;
+      }
+      return -1;
+    }
+    void flush(void) override {
+      for(int n=0;n<N;n++) inputs[n]->flush();
+    }
+    size_t write(uint8_t v) override {
+      for(int n=0;n<N;n++) inputs[n]->write(v);
+      return 1;
+    }
+  };
+
 
   }//namespace Menu
 
